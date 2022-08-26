8f721cecf8f19d964c75310e364bced5125eb766
Ethan Groene <ethangroene@gmail.com>
2022-08-25 23:44:00 -0500

    Add id to modal container; assign data-open & -close
    Also assign the accessing of these data-animations to vars

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const modalOpen = '[data-open]';
+const modalClose = '[data-close]';
+
+const openModal = document.querySelectorAll(modalOpen);
+const closeModal = document.querySelectorAll(modalClose);
