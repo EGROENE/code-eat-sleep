5fb5d980a8a69024168bd44eaa37fc3a7609906e
Ethan Groene <ethangroene@gmail.com>
2022-08-26 01:14:28 -0500

    Correct syntax for making modals visible upon click

--- js/main.js
+++ js/main.js
@@ -8,8 +8,8 @@ const closeModal = document.querySelectorAll(modalClose);
 // Full site modal 'open buttons'
 for (const elem of openModal) {
     elem.addEventListener('click', function() {
-        const modalId = this.dataset.open; // this refers to parent element, which is elem here. It then accesses datasets that are followed by 'open'. Don't use arrow func here, so that this keyword works.
-        document.getElementById(about.classList.add(isVisible)); // this adds .is-visible as a class to everything in document with data-open="about"
+        const modalId = this.dataset.open; // this refers to parent element, which is elem here. It then accesses datasets that are followed by 'open'. Don't use arrow func here, so that this keyword works;
+        document.getElementById(modalId).classList.add(isVisible); // this adds .is-visible as a class to everything in document with data-open="about"
     })
 }
 
